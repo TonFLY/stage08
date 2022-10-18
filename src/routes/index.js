a2307023e4f8c8444b2091dfa3c44ba3d625e37e
tonFLY <techwellington.dev@gmail.com>
2022-10-18 19:18:15 -0300

    final update

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
 const usersRouter = require("./users.routes");
 const notesRoutes = require("./notes.routes");
+const tagsRoutes = require("./tags.routes");
 
 const routes = Router();
 
+routes.use("/tags", tagsRoutes);
 routes.use("/users", usersRouter);
 routes.use("/notes", notesRoutes);
 
